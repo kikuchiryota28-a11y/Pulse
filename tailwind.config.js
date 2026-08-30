d74d3869a41157eaf9bc979ddd3301e1df3ef04a
R 9999 <kikuchiryota28@gmail.com>
2026-08-30 23:20:55 +0900

    Remove unnecessary Tailwind v3 config

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: { fontFamily: { sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'], mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'monospace'] } } },
-  plugins: [],
-};
