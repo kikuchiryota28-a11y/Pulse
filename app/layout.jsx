2341a51e822ae30c3a5791559c0f62ecd6e16727
R 9999 <kikuchiryota28@gmail.com>
2026-09-09 23:16:08 +0900

    style(ui): load Pulse reveal art direction globally

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "../src/pulse-social.css";
 import "../src/pulse-ui-v1.css";
 import "../src/pulse-art-direction-v2.css";
+import "../src/pulse-reveal-v2.css";
 import PulseMotion from "./PulseMotion";
 import MoveExperienceBridge from "../components/MoveExperienceBridge";
 import AuthGate from "../components/AuthGate";
