fd42c78c05daff56ad45ad36ba468b64a3d359a9
R 9999 <kikuchiryota28@gmail.com>
2026-09-09 23:15:06 +0900

    style(ui): load Pulse spatial art direction globally

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "../src/pulse-social.css";
 import "../src/pulse-ui-v1.css";
+import "../src/pulse-art-direction-v2.css";
 import PulseMotion from "./PulseMotion";
 import MoveExperienceBridge from "../components/MoveExperienceBridge";
 import AuthGate from "../components/AuthGate";
