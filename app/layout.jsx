3ac8752f8b909cea3e504631768d69a689dd9d9c
R 9999 <kikuchiryota28@gmail.com>
2026-09-10 05:53:56 +0900

    style(ui): load simplified Pulse interaction layer

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "../src/pulse-ui-v1.css";
 import "../src/pulse-art-direction-v2.css";
 import "../src/pulse-reveal-v2.css";
 import "../src/pulse-playful-spatial.css";
+import "../src/pulse-ui-v3.css";
 import PulseMotion from "./PulseMotion";
 import MoveExperienceBridge from "../components/MoveExperienceBridge";
 import AuthGate from "../components/AuthGate";
