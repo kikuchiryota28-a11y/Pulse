d95b80bd9445d3aad2ae16d85f1e1a5f2e0da49c
R 9999 <kikuchiryota28@gmail.com>
2026-09-10 19:14:22 +0900

    Load final Fluid Play screen layer

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,7 @@ import "../src/pulse-content-focus.css";
 import "../src/pulse-design-system.css";
 import "../src/pulse-fluid-polish.css";
 import "../src/pulse-fluid-components.css";
+import "../src/pulse-fluid-screens.css";
 import PulseMotion from "./PulseMotion";
 import MoveExperienceBridge from "../components/MoveExperienceBridge";
 import AuthGate from "../components/AuthGate";
